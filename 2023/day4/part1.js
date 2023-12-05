3c947f5866f3ff9a265f205619b94ec08c9a08dc
Nathan Harper <nathan.sam.harper@gmail.com>
2023-12-05 12:03:39 -0500

    d4p2

--- 2023/day4/part1.js
+++ 2023/day4/part1.js
@@ -1,12 +1,10 @@
 const rows = require('fs').readFileSync('./data.txt').toString().split("\n").filter(x => x);
 
-function stringListToNumberArray(str) {
-  return str.trim().split(/\s+/).map(x => Number(x));
-}
-
 const total = rows.reduce((sum, row) => {
   const [, cardData] = row.split(': ');
-  const [winningNumbers, myNumbers] = cardData.split(' | ').map(stringListToNumberArray);
+  const [winningNumbers, myNumbers] = cardData.split(' | ').map((str) => {
+    return str.trim().split(/\s+/);
+  });
   const intersection = myNumbers.filter(num => winningNumbers.includes(num));
 
   if (intersection.length < 1) {
