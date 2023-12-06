544d16c275282b68bb2d891ee607c5e0cbe6a70c
Nathan Harper <nathan.sam.harper@gmail.com>
2023-12-06 08:51:05 -0500

    d6

---
+++ 2023/day6/part1.js
@@ -0,0 +1,20 @@
+const lines = require('fs').readFileSync('./data.txt').toString().split("\n").filter(x => x);
+const getNums = str => str.match(/\d+/g).map(n => Number(n));
+const zip = (a, b) => a.map((x, i) => [ x, b[i] ]);
+const times = getNums(lines.shift());
+const distances = getNums(lines.shift());
+const pairs = zip(times, distances);
+
+const result = pairs.reduce((acc, [time, distance]) => {
+  let ways = 0;
+  for (let i = 1; i < time; i++) {
+    if ((time - i) * i > distance) {
+      ways++;
+    } else if (ways) {
+      break;
+    }
+  }
+  return acc * ways;
+}, 1);
+
+console.log(result);
---
+++ 2023/day6/part2.js
@@ -0,0 +1,15 @@
+const lines = require('fs').readFileSync('./data.txt').toString().split("\n").filter(x => x);
+const getNum = str => str.replace(/\s+/g, '').match(/\d+/g).map(n => Number(n)).shift();
+const time = getNum(lines.shift());
+const distance = getNum(lines.shift());
+
+let ways = 0;
+for (let i = 1; i < time; i++) {
+  if ((time - i) * i > distance) {
+    ways++;
+  } else if (ways) {
+    break;
+  }
+}
+
+console.log(ways);
