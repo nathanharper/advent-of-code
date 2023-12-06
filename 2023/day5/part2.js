0bb6f62ecfdb0db00a69ab3e24cc7b72c5862b44
Nathan Harper <nathan.sam.harper@gmail.com>
2023-12-05 19:44:58 -0500

    d5p2

---
+++ 2023/day5/part2.js
@@ -0,0 +1,42 @@
+const lines = require('fs').readFileSync('./data.txt').toString().split("\n").filter(x => x);
+
+const getNums = str => str.match(/\d+/g).map(n => Number(n));
+const seedNums = getNums(lines.shift());
+const maps = [];
+
+(() => {
+  // conversion map setup
+  let i = 0;
+  while (i++ < lines.length) {
+    const conversions = [];
+    while (i < lines.length && /^\d/.test(lines[i])) {
+      conversions.push(getNums(lines[i++]));
+    }
+    maps.push(conversions);
+  }
+})();
+
+function findLocation(seed) {
+  return maps.reduce((key, mappers) => {
+    for (let i = 0; i < mappers.length; i++) {
+      const [destBase, sourceBase, range] = mappers[i];
+      if (key >= sourceBase && key <= sourceBase + range - 1) {
+        return destBase + (key - sourceBase);
+      }
+    }
+    return key;
+  }, seed);
+}
+
+let min = Infinity;
+for (let i = 0; i < seedNums.length; i++) {
+  const start = seedNums[i];
+  const range = seedNums[++i];
+
+  for (let x = 0; x < range; x++) {
+    const seed = x + start;
+    min = Math.min(min, findLocation(seed));
+  }
+}
+
+console.log(min);
