c3b92d4bd7e49070d7921a834f16dabae0bc92fc
Nathan Harper <nathan.sam.harper@gmail.com>
2023-12-08 22:31:02 -0500

    d1p1 in rust

---
+++ 2023/day1/part1.rs
@@ -0,0 +1,44 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn find_nums(w: &str) -> std::option::Option<(char, char)> {
+    let mut vec = vec![];
+    for chr in w.chars() {
+        if
+            chr == '1'
+            || chr == '2'
+            || chr == '3'
+            || chr == '4'
+            || chr == '5'
+            || chr == '6'
+            || chr == '7'
+            || chr == '8'
+            || chr == '9'
+        {
+            vec.insert(vec.len(), chr);
+        }
+    }
+    Some((vec[0], vec[vec.len() - 1]))
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let data_file: &String = &args[1];
+    let mut file = File::open(data_file)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let rows = contents.split("\n");
+    let total = rows.fold(0, |sum, txt| {
+        if txt.len() == 0 { return sum }
+        let pair = find_nums(txt);
+        let num_str = match pair {
+            Some((x, y)) => Ok(format!("{}{}", x, y)),
+            _ => Err("fuck you"),
+        };
+        let combined_int = num_str.unwrap().parse::<i32>().unwrap();
+        sum + combined_int
+    });
+    println!("{}", total);
+    Ok(())
+}
